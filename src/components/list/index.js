38c92ce329cbdf003628ce0d21e184a05b3aea32
aidanakmatova13 <akmatova.don@gmail.com>
2021-09-12 05:46:37 +0300

    fix code

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -8,18 +8,16 @@ const List = ({item, delTodo, updateTodo}) =>{
         setEdit(false)
     }
     return(
-            <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+            <li className="list-group-item d-flex justify-content-between align-items-center">
                 {
                     edit ? <input onChange={(e) => setNewChange(e.target.value)} defaultValue={item.title} type='text'/> : <span>{item.title}</span>
                 }
-                <div>
                     <button onClick={() => edit ? handleSave() : setEdit(true)} type='button' className='btn btn-warning me-2 btn-sm'>
                     {
-                        edit ? <span>Save</span> : <span>Edit</span>
+                        edit ? "Save" : "Edit"
                     }
                     </button>
                     <button onClick={() => delTodo(item.id)} type='button' className='btn btn-danger btn-sm'>Delete</button>
-                </div>
             </li>
     )
 }
--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -1,45 +1,48 @@
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import List from "../list";
+import axios from "axios";
 
 const Todo = () =>{
-    const [todos, setTodos] = useState([
-        {id: 1, title: 'Сходить в магазин', createdAt: + new Date()},
-        {id: 2, title: 'Сьесть мороженку', createdAt: + new Date()},
-        {id: 3, title: 'Сходить в парк', createdAt: + new Date()}
-    ])
+    const [todos, setTodos] = useState([])
     const [newTodo, setNewTodo] = useState('')
-    const handleChange = (e) =>{
-        setNewTodo(e.target.value)
-    }
     const delTodo = (id) =>{
-        setTodos(todos.filter(el => el.id !== id))
+        axios.delete(`https://613d36a694dbd600172ab88f.mockapi.io/api/todos/${id}`)
+            .then(() => setTodos(todos.filter(el => el.id !== id)))
     }
     const addTodo =() =>{
         const newItem ={
-            id: todos.length ? todos[todos.length-1].id+1 : 1,  //проверка на undefined
             title:newTodo,
             createdAt: + new Date()
         }
-        setTodos([...todos, newItem])
-        setNewTodo('')
-
+        axios.post(`https://613d36a694dbd600172ab88f.mockapi.io/api/todos/`, newItem)
+            .then(({data}) => {
+                setTodos([...todos, data])
+                setNewTodo('')
+            })
     }
     const updateTodo = (modified, id) => {
         setTodos(todos.map(item => item.id === id ? {...item, title: modified} : item    ))
     }
+    useEffect(() =>{
+        axios('https://613d36a694dbd600172ab88f.mockapi.io/api/todos')
+            .then(({data}) => setTodos(data))
+    },[])
+
+
+
     return(
         <div className='row offset-md-3 my-5'>
             <div className='col-md-4'>
                 <div className='d-flex mb-4'>
-                    <input onChange={handleChange} value={newTodo} onKeyDown={e => {if(e.key === "Enter") addTodo()}} type="text" className='form-control me-2'/>
+                    <input onChange={(e)=>setNewTodo(e.target.value)} value={newTodo} onKeyDown={e => {if(e.key === "Enter") addTodo()}} type="text" className='form-control me-2'/>
                     <button onClick={addTodo} disabled={!newTodo.trim()} type="button" className='btn btn-primary'>Добавить</button>
                 </div>
                 <ul className="list-group">
                     {
                         todos.map(item =>
-                            <List item={item} delTodo={delTodo} updateTodo={updateTodo}/>
+                            <List key={item.id} item={item} delTodo={delTodo} updateTodo={updateTodo}/>
                         )
                     }
                 </ul>
